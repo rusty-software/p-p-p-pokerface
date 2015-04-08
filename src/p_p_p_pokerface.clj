5ea79744eb9095b7618d5d14f0c74e8c02b64668
Rusty Bentley <mycoding.alert@gmail.com>
2015-04-08 10:00:11 -0500

    everything passing

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -11,16 +11,16 @@
   (let [[_ s] card]
     (str s)))
 
-(defn ^:private kv-frequencies [kv hand]
+(defn- kv-frequencies [kv hand]
   (frequencies (map kv hand)))
 
-(defn ^:private rank-frequencies [hand]
+(defn- rank-frequencies [hand]
   (kv-frequencies rank hand))
 
-(defn ^:private suit-frequencies [hand]
+(defn- suit-frequencies [hand]
   (kv-frequencies suit hand))
 
-(defn ^:private proper-size-and-rank? [hand size rank-count]
+(defn- proper-size-and-rank? [hand size rank-count]
   (let [rank-counts (vals (rank-frequencies hand))]
     (and (= size (count rank-counts))
          (not (nil? (some #{rank-count} rank-counts))))))
@@ -44,11 +44,28 @@
 (defn two-pairs? [hand]
   (proper-size-and-rank? hand 3 2))
 
+(defn- maybe-replace-ace [ranks]
+  (sort (replace {14 1} ranks)))
+
 (defn straight? [hand]
-  nil)
+  (let [ordered-hand-ranks (sort (map rank hand))
+        low-rank (apply min ordered-hand-ranks)
+        expected-straight (range low-rank (+ low-rank 5))
+        low-straight (range 1 6)]
+    (or (= ordered-hand-ranks expected-straight)
+        (= (maybe-replace-ace ordered-hand-ranks) low-straight))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
